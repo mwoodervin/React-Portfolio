6499c31e091f45288635d87d9ad99135e04494f3
Mary Wood <mwoodervin@gmail.com>
2020-11-05 21:48:03 -0500

    added classname to home/about page for future styling

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -3,25 +3,25 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-              <div class="row">
-      <div className="col-lg-3">
-        <img className="img img-thumbnail" src="./assets/ProfilePic.jpg" />
-        <button><a href="https://www.dropbox.com/s/o6cou9fcufy3opy/Mary%20Ervin%20Resume_August%202020.doc?dl=0">Link to full résumé</a></button>
-      </div>
-      <div className="col-lg-9">
-        <p>I am an independent consultant specializing in small business development, management and systems improvement.</p> 
-          
-        <p>I have a background in finance and am currently two-thirds of the way through a 6-month coding bootcamp, working toward a certificate in full-stack web development.
-            I am passionate about creating strong, healthy, and collaborative communities that function at a high level and focus around a set of values, product offerings, and/or innovative programs. </p>
+            <div class="row">
+                <div className="col-lg-3">
+                    <img className="img img-thumbnail" src="./assets/ProfilePic.jpg" />
+                    <button><a href="https://www.dropbox.com/s/o6cou9fcufy3opy/Mary%20Ervin%20Resume_August%202020.doc?dl=0">Link to full résumé</a></button>
+                </div>
+                <div className="col-lg-9 aboutStyle">
+                    <p>I am an independent consultant specializing in small business development, management and systems improvement.</p>
 
-        <p>Areas of professional experience include: Strategic Planning and Organizational Analysis ▪ Project Management and Process Development ▪ Collaborative Problem Solving ▪ Leadership, Coaching, Team Building & Training ▪ Conflict Resolution & Consensus Building ▪ Performance Management ▪ Business Culture Development ▪ Innovative Program Development ▪ Presentations / Public Speaking  ▪  Budget Development   </p>
+                    <p>I have a background in finance and am currently two-thirds of the way through a 6-month coding bootcamp, working toward a certificate in full-stack web development.
+                        I am passionate about creating strong, healthy, and collaborative communities that function at a high level and focus around a set of values, product offerings, and/or innovative programs. </p>
 
-        <p>I have served on several school and community boards and I was a founding board member of Catamount Womenaid, a community-based organization dedicated to providing short-term grant funding for families in need.</p>
-      
-        <p>I graduated from Magna Cum Laude from Hamilton College and hold an MBA with a concentration in Finance from the University at Albany.  I am living in Stratham, NH with my two sons.
-          My spare time is spent outdoors as much as possible!</p>
-      </div>
-    </div>
+                    <p>Areas of professional experience include: Strategic Planning and Organizational Analysis ▪ Project Management and Process Development ▪ Collaborative Problem Solving ▪ Leadership, Coaching, Team Building & Training ▪ Conflict Resolution & Consensus Building ▪ Performance Management ▪ Business Culture Development ▪ Innovative Program Development ▪ Presentations / Public Speaking  ▪  Budget Development   </p>
+
+                    <p>I have served on several school and community boards and was a founding board member of Catamount Womenaid, a community-based organization dedicated to providing short-term grant funding for families in need.</p>
+
+                    <p>I graduated from Magna Cum Laude from Hamilton College and hold an MBA with a concentration in Finance from the University at Albany.  I am living in Stratham, NH with my two sons.
+                        My spare time is spent outdoors as much as possible!</p>
+                </div>
+            </div>
 
         </div>
     )
