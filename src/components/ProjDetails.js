386c03339424902f7013793292b03dd4d1a89f32
Mary Wood <mwoodervin@gmail.com>
2020-11-08 14:49:14 -0500

    populated ProjDetails with  project information

--- src/components/ProjDetails.js
+++ src/components/ProjDetails.js
@@ -4,65 +4,66 @@ export const projDetails = [
         "description": "In the age of work-from-anywhere business, builing a connected workforce is more challenging than ever and no less important. This is a team building application targeting remote work groups. The Team Builder app leverages team member profiles to create cutomized, engaging and productive activities that lead to stronger and more productive work groups.",
         "appLink": "https://the-team-builder.herokuapp.com/",
         "codeLink": "https://github.com/mwoodervin/the-team-builders",
-        "tech": "Technologies: HTML, CSS. jQuery, MySWL, Sequelize, ExpressJS, Express Handlebars, Axios, Auth0",
-        "imgURL": "src/assets/img/teambuildersscreenshot.png",
-        "imgAlt": "Team Builder Logo",
-        "component": "Project"
-    },
-
-    {
-        "title": "Team Builder",
-        "description": "In the age of work-from-anywhere business, builing a connected workforce is more challenging than ever and no less important. This is a team building application targeting remote work groups. The Team Builder app leverages team member profiles to create cutomized, engaging and productive activities that lead to stronger and more productive work groups.",
-        "appLink": "https://the-team-builder.herokuapp.com/",
-        "codeLink": "https://github.com/mwoodervin/the-team-builders",
-        "tech": "Technologies: HTML, CSS. jQuery, MySWL, Sequelize, ExpressJS, Express Handlebars, Axios, Auth0",
-        "imgURL": "src/assets/img/teambuildersscreenshot.png",
-        "imgAlt": "Team Builder Logo",
-        "component": "Project"
-    },
-
-    {
-        "title": "Team Builder",
-        "description": "In the age of work-from-anywhere business, builing a connected workforce is more challenging than ever and no less important. This is a team building application targeting remote work groups. The Team Builder app leverages team member profiles to create cutomized, engaging and productive activities that lead to stronger and more productive work groups.",
-        "appLink": "https://the-team-builder.herokuapp.com/",
-        "codeLink": "https://github.com/mwoodervin/the-team-builders",
-        "tech": "Technologies: HTML, CSS. jQuery, MySWL, Sequelize, ExpressJS, Express Handlebars, Axios, Auth0",
+        "tech": "Technologies: HTML, CSS, jQuery, MySWL, Sequelize, ExpressJS, Express Handlebars, Axios, Auth0",
         "imgURL": "../../img/teambuildersscreenshot.png",
         "imgAlt": "Team Builder Logo",
         "component": "Project"
     },
 
     {
-        "title": "Team Builder",
-        "description": "In the age of work-from-anywhere business, builing a connected workforce is more challenging than ever and no less important. This is a team building application targeting remote work groups. The Team Builder app leverages team member profiles to create cutomized, engaging and productive activities that lead to stronger and more productive work groups.",
-        "appLink": "https://the-team-builder.herokuapp.com/",
-        "codeLink": "https://github.com/mwoodervin/the-team-builders",
-        "tech": "Technologies: HTML, CSS. jQuery, MySWL, Sequelize, ExpressJS, Express Handlebars, Axios, Auth0",
-        "imgURL": "src/assets/img/teambuildersscreenshot.png",
-        "imgAlt": "Team Builder Logo",
+        "title": "Flick the Deck",
+        "description": "What if choosing a movie on Friday night were as effortless as picking a card ... any card? Flick-the-Deck is a lighthearted application that combines the ease of picking a card with user input to generate movie suggestions. All you need to do is click to shuffle, choose max runtime, rating and genre ... the rest is luck of the draw!",
+        "appLink": "https://mwoodervin.github.io/Flick-the-Deck/",
+        "codeLink": "https://github.com/mwoodervin/Flick-the-Deck",
+        "tech": "Technologies: HTML, CSS, JavaScript, JQuery",
+        "imgURL": "../../img/FlickTheDeck.png",
+        "imgAlt": "Flick the Deck",
         "component": "Project"
     },
 
     {
-        "title": "Team Builder",
-        "description": "In the age of work-from-anywhere business, builing a connected workforce is more challenging than ever and no less important. This is a team building application targeting remote work groups. The Team Builder app leverages team member profiles to create cutomized, engaging and productive activities that lead to stronger and more productive work groups.",
-        "appLink": "https://the-team-builder.herokuapp.com/",
-        "codeLink": "https://github.com/mwoodervin/the-team-builders",
-        "tech": "Technologies: HTML, CSS. jQuery, MySWL, Sequelize, ExpressJS, Express Handlebars, Axios, Auth0",
-        "imgURL": "src/assets/img/teambuildersscreenshot.png",
-        "imgAlt": "Team Builder Logo",
+        "title": "Weather Dashboard",
+        "description": "Use this weather app to find today's weather and 5-day forecast for any US city. The site provides information about temperature, humidity, wind and UV index. Searched cities are saved for easy reference.",
+        "appLink": "https://mwoodervin.github.io/Weather-Dashboard/",
+        "codeLink": "https://github.com/mwoodervin/Weather-Dashboard",
+        "tech": "Technologies: HTML, CSS, JavaScript, jQuery",
+        "imgURL": "../../img/WeatherDashboard.png",
+        "imgAlt": "Weather Dashboard",
         "component": "Project"
     },
 
     {
-        "title": "Team Builder",
-        "description": "In the age of work-from-anywhere business, builing a connected workforce is more challenging than ever and no less important. This is a team building application targeting remote work groups. The Team Builder app leverages team member profiles to create cutomized, engaging and productive activities that lead to stronger and more productive work groups.",
-        "appLink": "https://the-team-builder.herokuapp.com/",
-        "codeLink": "https://github.com/mwoodervin/the-team-builders",
-        "tech": "Technologies: HTML, CSS. jQuery, MySWL, Sequelize, ExpressJS, Express Handlebars, Axios, Auth0",
-        "imgURL": "src/assets/img/teambuildersscreenshot.png",
-        "imgAlt": "Team Builder Logo",
+        "title": "Password Generator",
+        "description": "Need to create a robust passowrd? This is the place. Based on a set of user-selectd criteria, this application will generate a random password consisting of between 8 and 128 characters.",
+        "appLink": "https://mwoodervin.github.io/Password-generator/",
+        "codeLink": "https://github.com/mwoodervin/Password-generator",
+        "tech": "Technologies: HTML, CSS, jQuery",
+        "imgURL": "../../img/Password.png",
+        "imgAlt": "Password Generator",
         "component": "Project"
-    }
+    },
+
+    {
+        "title": "Day Planner",
+        "description": "A classic 15-hour day planner to maximize time management and efficency. This day planner tracks tasks from 6AM through 11PM with color-coding to indicate past, current and future time blocks. ToDo items can be saved and will display when page is reloaded.",
+        "appLink": "https://mwoodervin.github.io/Day-Planner/",
+        "codeLink": "https://github.com/mwoodervin/Day-Planner",
+        "tech": "Technologies: HTML, CSS. jQuery",
+        "imgURL": "../../img/DayPlanner.png",
+        "imgAlt": "Day Planner",
+        "component": "Project"
+    },
+
+    {
+        "title": "Yum Burger",
+        "description": "A light-hearted app to track your burger plans. This could be built out for more serious purposes including use by a niche burger bar-style restaurant to encourage patrons to keep track of the burgers they have tried and the ones they need to come back for.",
+        "appLink": "https://mighty-brook-63147.herokuapp.com/",
+        "codeLink": "https://github.com/mwoodervin/Yum-Burger",
+        "tech": "Technologies: HTML, CSS. jQuery",
+        "imgURL": "../../img/YumBurgers.png",
+        "imgAlt": "Yum Burger",
+        "component": "Project"
+    },
+
 ]
 
