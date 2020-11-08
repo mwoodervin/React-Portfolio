fc24b783af6f1bcb115bd38f5c80b59e592f9b10
Mary Wood <mwoodervin@gmail.com>
2020-11-08 14:25:03 -0500

    populated card with one set of project information

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { projDetails } from '../ProjDetails';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './project.css';
+
 
 import '../..//App.css';
 
@@ -12,23 +15,19 @@ export const Project = () => {
                 {projDetails.map((data, key) => {
                     return (
                         <div>
-                            <div key={key}>
-                                {data.title}
-                                {data.description}
-                                {data.appLink}
-                            </div>
-                            <CardDeck className="cardDeckStyle">
-                                <Card className="cardStyle">
-                                    <Card.Img variant="top" src="holder.js/100px160" />
+                            <CardDeck key={key} className="cardDeckStyle">
+                                <Card border='success' style = {{ width: '18rem' }} className="cardStyle">
+                                    <Card.Img variant="top" src={data.imgURL} alt={data.componentimgAlt}/>
                                     <Card.Body>
-                                        <Card.Title>Card title</Card.Title>
+                                        <Card.Title>{data.title}</Card.Title>
                                         <Card.Text>
-                                            This is a wider card with supporting text below as a natural lead-in to
-                                            additional content. This content is a little bit longer.
+                                           <p>{data.description}</p>
+                                           <p>{data.tech}</p>
                                         </Card.Text>
                                     </Card.Body>
                                     <Card.Footer>
-                                        <small className="text-muted">Last updated 3 mins ago</small>
+                                    <Button className="buttonStyle" variant="info"><a href= {data.appLink} />App</Button>
+                                    <Button className="buttonStyle" variant="info"><a href= {data.codeLink} />Code</Button>
                                     </Card.Footer>
                                 </Card>
                             </CardDeck>
