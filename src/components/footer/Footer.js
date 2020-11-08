6401617590bf99ae84c0fc448832532fe5ec30e5
Mary Wood <mwoodervin@gmail.com>
2020-11-08 13:18:14 -0500

    tweaked sticky footer - working now

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './footer.css';
 
 // this is a sticky footer - maybe change?
 
@@ -6,7 +7,7 @@ function Footer() {
 return (
     <div>
         <footer className="d-flex flex-column sticky-footer-wrapper">
-            <div className="container">
+            <div className="container footerContainer">
                 <span className="text-muted">Mary W. Ervin | About Me | November 2020</span>
             </div>
         </footer>
