044a6d520769d4e8bf4fc71d360496e1a1cff66e
Mary Wood <mwoodervin@gmail.com>
2020-11-08 13:39:15 -0500

    created a template card in project.js

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
 import { projDetails } from '../ProjDetails';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Card from 'react-bootstrap/Card';
+
 import '../..//App.css';
 
 export const Project = () => {
     return (
         <div>
-            <div className="project-container"></div>
-            {projDetails.map((data, key) => {
-                return (
-                    <div>
-                        <div key={key}>
-                            {data.title}
-                            {data.description}
-                            {data.appLink}
+            <div className="project-container">
+                {projDetails.map((data, key) => {
+                    return (
+                        <div>
+                            <div key={key}>
+                                {data.title}
+                                {data.description}
+                                {data.appLink}
+                            </div>
+                            <CardDeck className="cardDeckStyle">
+                                <Card className="cardStyle">
+                                    <Card.Img variant="top" src="holder.js/100px160" />
+                                    <Card.Body>
+                                        <Card.Title>Card title</Card.Title>
+                                        <Card.Text>
+                                            This is a wider card with supporting text below as a natural lead-in to
+                                            additional content. This content is a little bit longer.
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <small className="text-muted">Last updated 3 mins ago</small>
+                                    </Card.Footer>
+                                </Card>
+                            </CardDeck>
                         </div>
-
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </div>
     );
 }
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -7,9 +7,7 @@ export default function Portfolio() {
     return (
         <div>
             <div className="container container-fluid">
-                <p className = "portfolioContent">Portfolio Page</p>
                 <Project />
-                <button className = "buttonStyle">click me</button>
             </div>
         </div>
     )
