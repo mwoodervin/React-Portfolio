34f97859c5984cfae21320f38b328e4375defc26
Mary Wood <mwoodervin@gmail.com>
2020-11-09 10:18:45 -0500

    project card updates

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -15,23 +15,21 @@ export const Project = () => {
                 {projDetails.map((data, key) => {
                     return (
                         <div>
-                            <div className='container-fluid containerStyle'>
-                                <CardDeck key={key} className="cardDeckStyle row row-cols-1 row-cols-md-3">
-                                    <Card border='success' className="cardStyle col mb-4">
-                                        <Card.Img variant="top" className='img-responsive' src={data.imgURL} alt={data.componentimgAlt} />
-                                        <Card.Body>
-                                            <Card.Title>{data.title}</Card.Title>
-                                            <Card.Text>
-                                                <p>{data.description}</p>
-                                                <p>{data.tech}</p>
-                                            </Card.Text>
-                                        </Card.Body>
-                                        <Card.Footer>
-                                            <Button className="buttonStyle" variant="info"><a href={data.appLink} />App</Button>
-                                            <Button className="buttonStyle" variant="info"><a href={data.codeLink} />Code</Button>
-                                        </Card.Footer>
-                                    </Card>
-                                </CardDeck>
+                            <div className='containerStyle' key={key}>
+                                <Card className="cardStyle">
+                                    <Card.Img variant="top" className='img-responsive' src={data.imgURL} alt={data.componentimgAlt} />
+                                    <Card.Body>
+                                        <Card.Title>{data.title}</Card.Title>
+                                        <Card.Text>
+                                            <p>{data.description}</p>
+                                            <p>{data.tech}</p>
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <Button className="buttonStyle"><a href={data.appLink} />Try me</Button>
+                                        <Button className="buttonStyle"><a href={data.codeLink} />Code</Button>
+                                    </Card.Footer>
+                                </Card>
                             </div>
                         </div>
                     );
