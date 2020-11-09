b5e3514e9e14ea874d8ef2d97b8215a8af1d9ba5
Mary Wood <mwoodervin@gmail.com>
2020-11-09 10:55:26 -0500

    added CardDeck to project.js

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -4,15 +4,14 @@ import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './project.css';
-
-
-import '../..//App.css';
+import '../../App.css';
 
 export const Project = () => {
     return (
         <div className="project-container">
             {projDetails.map((data, key) => {
                 return (
+                    <CardDeck>
                     <Card key={key} className="cardStyle">
                         <Card.Img variant="top" className='img-responsive' src={data.imgURL} alt={data.componentimgAlt} />
                         <Card.Body>
@@ -27,6 +26,7 @@ export const Project = () => {
                             <Button className="buttonStyle"><a href={data.codeLink} />Code</Button>
                         </Card.Footer>
                     </Card>
+                    </CardDeck>
                 );
             })}
         </div>
